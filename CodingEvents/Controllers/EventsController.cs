57d1a9654b1ddc77e6c12088f67fd99c516392c9
shea cooke <SheaSCooke@gmail.com>
2021-07-20 14:19:13 -0700

    finished exercise requirements

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEvents.Controllers
+{
+    
+    public class EventsController : Controller
+    {
+        //private static List<string> Events = new List<string>();
+
+        private static Dictionary<string, string> Events = new Dictionary<string, string>();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            
+
+            ViewBag.events = Events;
+
+            return View();
+        }
+
+        [HttpGet] 
+        // /events/add
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/events/add")]
+        public IActionResult NewEvent(string name, string description)
+        {
+            Events[name]=description;
+            return Redirect("/events");
+        }
+    }
+}
