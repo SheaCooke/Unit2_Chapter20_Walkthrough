e1e6b8869a8366041ab685e14b09350de3c2b6f1
shea cooke <SheaSCooke@gmail.com>
2021-08-04 11:27:18 -0700

    finished studio

--- CodingEvents/Controllers/EventCategoryController.cs
+++ CodingEvents/Controllers/EventCategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using CodingEvents.Data;
 using CodingEvents.Models;
+using CodingEvents.ViewModles;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -25,6 +26,33 @@ namespace CodingEvents.Controllers
         {
             List<EventCategory> eventCategories = _context.EventCategory.ToList();
             return View(eventCategories);
+        }
+
+
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModel addEventCategoryViewModel = new AddEventCategoryViewModel();
+            return View(addEventCategoryViewModel);
+        }
+
+        [HttpPost]
+        //[Route("/create")]
+        public IActionResult /*ProcessCreateEventCategoryForm*/Create(AddEventCategoryViewModel addEventCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                EventCategory newEventCategory = new EventCategory
+                {
+                    Name = addEventCategoryViewModel.Name
+                };
+                _context.EventCategory.Add(newEventCategory);
+                _context.SaveChanges();
+                return Redirect("Index");
+            }
+
+            return View();
+
+
         }
     }
 }
---
+++ CodingEvents/ViewModles/AddEventCategoryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEvents.ViewModles
+{
+    
+    public class AddEventCategoryViewModel
+    {
+
+        [Required(ErrorMessage ="This field is required.")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage ="Must be between 3 and 20 characters.")]
+        public string Name { get; set; }
+
+
+}
+}
